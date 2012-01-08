0576dd3b6b6a56c488e74f6f7fc064765af734d2
John Schimmel <john.schimmel@gmail.com>
2012-01-08 10:36:58 -0500

    static directory

--- web.js
+++ web.js
@@ -7,15 +7,15 @@ require('./models.js').buildModels(Schema, mongoose);
 var Post = mongoose.model('BlogPost');
 
 var app = express.createServer(express.logger());
-var db = mongoose.connect('mongodb://heroku_app1873269:55kp21eun4fle1ngb0n6l02p2q@ds029217.mongolab.com:29217/heroku_app1873269');
+var db = mongoose.connect('mongodb://heroku_app2392187:tcmrbi5pr5a3pqdp7193jo1035@dbh70.mongolab.com:27707/heroku_app2392187');
 
 
 app.get('/', function(request, response) {
     
-    var newPost = new Post();
-    newPost.title = "testing abc abc";
-    newPost.comments.push({title:'TEST comment'});
-    newPost.save();
+    //var newPost = new Post();
+    //newPost.title = "testing abc abc";
+    //newPost.comments.push({title:'TEST comment'});
+    //newPost.save();
 
     var query = Post.find({});
     query.exec(function (err, docs) {
@@ -30,6 +30,17 @@ app.get('/', function(request, response) {
     
     response.send('Hello World!!!');
 
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
 });
 
 var port = process.env.PORT || 3000;
