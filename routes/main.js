b98e129ceac18509c48aab71465fa269371af695
John Schimmel <john.schimmel@gmail.com>
2012-01-14 18:23:59 -0500

    testing out new route idea.

--- routes/main.js
+++ routes/main.js
@@ -1,32 +1,3 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-var hogan=require('hogan.js'),
-	adapter=require('./hogan-express.js');
-
-require('./models.js').buildModels(Schema, mongoose);
-var Post = mongoose.model('BlogPost');
-var ClassNote = mongoose.model('ClassNote');
-
-var app = express.createServer(express.logger());
-app.configure( function(){
-    app.db = mongoose.connect('mongodb://heroku_app2392187:tcmrbi5pr5a3pqdp7193jo1035@dbh70.mongolab.com:27707/heroku_app2392187');
-    app.use(express.bodyParser());
-    
-    app.use(express.static(__dirname + '/public')); // all static files (css, js, and IMGs) go in here
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    
-    // use Hogan.js as template engine. Info http://allampersandall.blogspot.com/2011/12/hoganjs-expressjs-nodejs.html
-    app.set('view engine','hogan.js'); 
-    app.set('views',__dirname+ '/views'); // use /views as template directory
-    app.set('view options',{layout:true}); // use layout.html w/ {{{body}}}
-    app.register('html',adapter.init(hogan)); //use .html files in /views
-    
-});
-
-require('./routes');
-
 /*************** ADMIN HANDLERS *************************/
 app.get('/tehSystem', function(request, response) {
     response.render("admin/index.html",{layout:'layouts/adminLayout'});
@@ -108,10 +79,4 @@ app.get("/",function(request,response) {
         sidebar : "the sidebar",
     });
     
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-
 });
