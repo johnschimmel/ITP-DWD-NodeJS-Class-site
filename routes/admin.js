cca1c7fe9419746c44a394d5305d25e1b34cd044
John Schimmel <john.schimmel@gmail.com>
2012-01-15 23:35:09 -0500

    working updating form

--- routes/admin.js
+++ routes/admin.js
@@ -37,21 +37,6 @@ app.get('/tehSystem/classnotes/entry', function(request, response) {
     });
 });
 
-app.get('/tehSystem/classnotes/edit/:urltitle', function(request, response) {
-    
-    //get class notes
-    ClassNote.findOne({urltitle:request.params.urltitle},function(err,doc){
-        response.render("admin/updateEntry.html",{
-            classnote : doc,
-            publishStatus:function(char) {
-                return "selected=selected";
-            },
-            layout:'layouts/adminLayout'
-        });
-    });
-    
-    
-});
 
 app.post('/tehSystem/classnotes/entry', function(request, response) {
 
@@ -70,12 +55,48 @@ app.post('/tehSystem/classnotes/entry', function(request, response) {
         
         newEntry.save();
         
-        for(n in request.body.entry) {
-            data[n] = request.body.entry[n];
-        }
-        console.log(data);
     }
-    
 
-    response.send("ok");
+    response.redirect("/tehSystem/classnotes/edit/" + newEntry.urltitle);
 });
+
+app.get('/tehSystem/classnotes/edit/:urltitle', function(request, response) {
+    
+    //get class notes
+    ClassNote.findOne({urltitle:request.params.urltitle},function(err,doc){
+        response.render("admin/updateEntry.html",{
+            classnote : doc,
+            publishStatus:function(char) {
+                return "selected=selected";
+            },
+            layout:'layouts/adminLayout'
+        });
+    });
+});
+
+app.post('/tehSystem/classnotes/edit/:urltitle', function(request, response) {
+    
+    //get class notes
+    var updateData = {
+        classdate : new Date(request.body.entry.classdate),
+        title : request.body.entry.title,
+        urltitle : request.body.entry.urltitle,
+        intro : request.body.entry.intro,
+        notes : request.body.entry.notes,
+        notesReady : request.body.entry.notesready, 
+        assignment : request.body.entry.assignment,
+        publishstatus : request.body.entry.publishstatus != undefined ? request.body.entry.publishstatus : 'draft'
+    };
+    
+    ClassNote.update({urltitle:request.params.urltitle},updateData,function(err,doc){
+        if (err) {
+            console.log("- - - - - - - - - -");
+            console.log(err);
+            console.log("- - - - - - - - - -");
+        } else {
+            response.redirect("/tehSystem/classnotes/edit/" + updateData.urltitle);
+        }
+    });
+
+});
+
